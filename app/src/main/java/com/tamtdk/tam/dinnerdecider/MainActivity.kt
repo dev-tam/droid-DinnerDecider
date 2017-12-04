02854ea16a9e58de894b1a33fac04b9bcf89707f
TamjidMd <tamjid.mdh@gmail.com>
2017-12-05 00:44:08 +0600

    Add food and Decide function.

--- app/src/main/java/com/tamtdk/tam/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/tamtdk/tam/dinnerdecider/MainActivity.kt
@@ -2,11 +2,30 @@ package com.tamtdk.tam.dinnerdecider
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val foodList = arrayListOf("Chinese","Hamburger","Pizza","McDonalds","Barros Pizza")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        decideBtn.setOnClickListener {
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+            Log.d("Dinner","Decide button pressed")
+            selectedFoodTxt.text = foodList[randomFood]
+        }
+
+        addFoodBtn.setOnClickListener {
+            val newFood = addFoodText.text.toString()
+            foodList.add(newFood)
+            Log.d("Dinner", foodList.toString())
+            addFoodText.text.clear()
+        }
     }
 }
